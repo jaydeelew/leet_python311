2bbfa3edbbf985872b3a011bad220ccf6414ee6e
jaydeelew <jaydeelew@gmail.com>
2023-06-25 23:45:07 -0700

    moving all hashing files to hashing

---
+++ Hashing/DestinationCity.py
@@ -0,0 +1,10 @@
+# You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+# Return the destination city, that is, the city without any path outgoing to another city.
+# It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+
+class Solution:
+    def destCity(self, paths: list[list[str]]) -> str:
+
+
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]] # output: Sao Paulo
