2042fca30ce0166c81191efba52e33f12df45ba4
jaydeelew <jaydeelew@gmail.com>
2023-11-19 14:17:21 -0800

    Another Greedy Algo

--- Greedy/LeastUniqueIntsAfterKremovals.py
+++ Greedy/LeastUniqueIntsAfterKremovals.py
@@ -1,4 +1,4 @@
-# Example 4: 1481. Least Number of Unique Integers after K Removals
+# 1481. Least Number of Unique Integers after K Removals
 # Given an array of integers arr and an integer k,
 # find the least number of unique integers after removing exactly k elements.
 from collections import Counter
@@ -8,25 +8,28 @@ class Solution:
     def findLeastNumOfUniqueInts(self, arr: list[int], k: int) -> int:
         counts = Counter(arr)
         # sorted_counts = sorted(counts.items(), key=lambda x: x[1])
-        sorted_counts = sorted(counts.values())
+        sorted_counts = sorted(counts.values())  # sorted returns a list in ascending order
         ans = len(counts)
-        i = 0
 
-        while k:
-
+        for cnt in sorted_counts:
+            if cnt <= k:
+                ans -= 1
+                k -= cnt
+            else:
+                return ans
         return ans
 
 
-arr = [5, 5, 4]
-k = 1 w
-# Output: 1
+# arr = [5, 5, 4]
+# k = 1
+# # Output: 1
 
 # arr = [4, 3, 1, 1, 3, 3, 2]
 # k = 3
 # # Output: 2
 
-# arr = [1]
-# k = 1
+arr = [1]
+k = 1
 
 sol = Solution()
 print(sol.findLeastNumOfUniqueInts(arr, k))
