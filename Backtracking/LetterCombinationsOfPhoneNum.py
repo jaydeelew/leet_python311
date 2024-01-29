fb69faf3f821b750fb3897b52fb31e46ee861fd3
jaydeelew <jaydeelew@gmail.com>
2024-01-28 23:15:33 -0800

    initial

---
+++ Backtracking/LetterCombinationsOfPhoneNum.py
@@ -0,0 +1,61 @@
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+# Return the answer in any order.
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits:
+            return []
+
+        buttons = [
+            ["a", "b", "c"],
+            ["d", "e", "f"],
+            ["g", "h", "i"],
+            ["j", "k", "l"],
+            ["m", "n", "o"],
+            ["p", "q", "r", "s"],
+            ["t", "u", "v"],
+            ["w", "x", "y", "z"],
+        ]
+
+        # use list comprehension to turn digits string into list of digits
+        digits_list = [int(x) for x in digits]
+
+        ans_list = []
+
+        def backtrack(curr: list[str], digits_list_idx: int):
+            if len(curr) == len(digits):
+                # be sure to append copy of curr and not reference to curr
+                ans_list.append(curr.copy())
+                return
+
+            # subtract 2 from element in digits list to translate from button to index in digits_list
+            for ltr in buttons[digits_list[digits_list_idx] - 2]:
+                curr.append(ltr)
+                backtrack(curr, digits_list_idx + 1)
+                curr.pop()
+
+        backtrack([], 0)
+        output = []
+
+        for lst in ans_list:
+            strg = ""
+            for letter in lst:
+                strg += letter
+            output.append(strg)
+
+        return output
+
+
+# digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+digits = ""
+# # Output: []
+
+# digits = "2"
+# Output: ["a","b","c"]
+
+sol = Solution()
+print(sol.letterCombinations(digits))
