50a615b8336ab9d0eec946a11bfe2d65044fdcb6
jaydeelew <jaydeelew@gmail.com>
2024-05-21 17:59:35 -0700

    renamed variables

--- AllTemplates/DijkstraAlgorithmAdjacencyList.py
+++ AllTemplates/DijkstraAlgorithmAdjacencyList.py
@@ -3,36 +3,37 @@ import heapq
 
 def dijkstra(adj_list, starting_node):
     # distances maintains the shortest distance from the starting node to all other vertices
-    total_distances = dict.fromkeys(adj_list, float("inf"))
-    total_distances[starting_node] = 0
+    distances = dict.fromkeys(adj_list, float("inf"))
+    distances[starting_node] = 0
 
     # list of tuples: (current minimum distance from start, current node)
     min_heap = [(0, starting_node)]
+    # seen is needed to prevent revisiting nodes that already have their shortest distance from start computed
     seen = set()
 
     while min_heap:
         # we use a min heap here to potentially reduce the number of times we update the distance of a node
-        # a FIFO queue could allow greater distances to be updated in total_distances first
+        # a FIFO queue could allow greater distances to be updated in distances first
         # whereas if a shorter distance is added first, greater distances will be ignored
-        curr_node_dist_from_start, current_node = heapq.heappop(min_heap)
+        dist_from_start, node = heapq.heappop(min_heap)
 
-        if current_node in seen:
+        if node in seen:
             continue
         # a seen neighbor's distance from start can still
-        # be updated in total_distances by the current node
-        seen.add(current_node)
+        # be updated in distances by the current node
+        seen.add(node)
 
         # for each neighbor of the current node, we calculate the distance from start to neighbor
-        for neighbor, dist_curr_node_to_neighbor in adj_list[current_node].items():
-            dist_start_to_neighbor = curr_node_dist_from_start + dist_curr_node_to_neighbor
+        for neighbor, dist_to_neighbor in adj_list[node].items():
+            dist_start_to_neighbor = dist_from_start + dist_to_neighbor
 
             # if the distance from start neighbor is shorter than the total distance recorded thus far,
-            # we update the distance in total_distances and push the neighbor onto the heap
-            if dist_start_to_neighbor < total_distances[neighbor]:
-                total_distances[neighbor] = dist_start_to_neighbor
+            # we update the distance in distances and push the neighbor onto the heap
+            if dist_start_to_neighbor < distances[neighbor]:
+                distances[neighbor] = dist_start_to_neighbor
                 heapq.heappush(min_heap, (dist_start_to_neighbor, neighbor))
 
-    return total_distances
+    return distances
 
 
 # adj_list = {
---
+++ AllTemplates/DijkstraAlgorithmAdjacencyMatrix.py
@@ -0,0 +1,49 @@
+import heapq
+
+
+def dijkstra(adj_matrix, starting_node):
+    # distances maintains the shortest distance from the starting node to all other vertices
+    num_nodes = len(adj_matrix)
+    distances = dict.fromkeys(range(num_nodes), float("inf"))
+    distances[starting_node] = 0
+
+    # list of tuples: (current minimum distance from start, current node)
+    min_heap = [(0, starting_node)]
+    # seen is needed to prevent revisiting nodes that already have their shortest distance from start computed
+    seen = set()
+
+    while min_heap:
+        # we use a min heap here instead of a FIFO queue to potentially reduce
+        # the number of times we update the distance of a node
+        dist_from_start, node = heapq.heappop(min_heap)
+
+        if node in seen:
+            continue
+        # a seen neighbor's distance from start can still
+        # be updated in distances by the current node
+        seen.add(node)
+
+        # for each neighbor of the current node, we calculate the distance from start to neighbor
+        for neighbor in range(num_nodes):
+            if adj_matrix[node][neighbor] != 0:  # There is a connection
+                dist_to_neighbor = adj_matrix[node][neighbor]
+                dist_start_to_neighbor = dist_from_start + dist_to_neighbor
+
+                # if the distance from start neighbor is shorter than the total distance recorded thus far,
+                # we update the distance in distances and push the neighbor onto the heap
+                if dist_start_to_neighbor < distances[neighbor]:
+                    distances[neighbor] = dist_start_to_neighbor
+                    heapq.heappush(min_heap, (dist_start_to_neighbor, neighbor))
+
+    return distances
+
+
+adj_matrix = [
+    [0, 8, 4, 0, 0],
+    [0, 0, 4, 0, 0],
+    [0, 3, 0, 2, 5],
+    [0, 0, 0, 0, 2],
+    [0, 0, 0, 0, 0],
+]
+
+print(dijkstra(adj_matrix, 0))
